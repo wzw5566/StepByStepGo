b461692b4d8ab5267931752861528f9534ce776e
奋斗的Vincent <wzw33874@qq.com>
2019-08-27 07:09:09 +0800

    切片共享内存与数组的区别

--- src/ch6/slice_test/slice_test.go
+++ src/ch6/slice_test/slice_test.go
@@ -13,7 +13,7 @@ func TestSliceInit(t *testing.T) {
 	t.Log(len(s0), cap(s0))
 
 	//初始化切片
-	s1 := []int{1,2,3,4}
+	s1 := []int{1, 2, 3, 4}
 	t.Log(len(s1), cap(s1))
 
 	//make 初始化切片，两个参数 len，cap 长度和容量
@@ -23,6 +23,33 @@ func TestSliceInit(t *testing.T) {
 
 	t.Log(s2[0], s2[1], s2[2])
 
+}
 
+//可变空间的切片
+func TestSliceGrowing(t *testing.T) {
+	s := []int{}
+	for i := 0; i < 10; i++ {
+		s = append(s, i)
+		t.Log(len(s), cap(s))
+	}
+}
+
+// 切片共享内存,当其中一个值改变，与其共享内存的其他变量也会改变
+func TestSliceShareMomery(t *testing.T) {
+	year := []string{"Jan", "Feb", "Mar", "Jun", "Jul", "Aug", "Seq", "Oct", "Nov", "Dec"}
+
+	Q2 := year[3:6]
+	t.Log(Q2, len(Q2), cap(Q2))
+
+	sunmmer := year[5:8]
+	t.Log(sunmmer, len(sunmmer), cap(sunmmer))
+
+	//修改切片的值
+	Q2[0] = "unknow"
+	t.Log(Q2)
+	t.Log(sunmmer)
+	t.Log(year)
 
 }
+
+
