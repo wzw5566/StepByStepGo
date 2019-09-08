d42fde1c1b2ca9454081c8c748ebdedf6e9a09a0
奋斗的Vincent <wzw33874@qq.com>
2019-09-09 06:24:35 +0800

    类型转转&常量

--- src/ch10/func/type_transformation/main.go
+++ src/ch10/func/type_transformation/main.go
@@ -5,11 +5,17 @@ import (
 	"math"
 )
 
+//Pi 常量的定义
+const Pi = 3.14
+
 func main() {
 	var x, y int = 3, 4
 	//显性类型转化
-	var f  = math.Sqrt(float64(x*x + y*y))
-	fmt.Printf("%T\n",f) // %T输出类型
-	var z  = uint(f) //显性类型转化 
+	var f = math.Sqrt(float64(x*x + y*y))
+	fmt.Printf("%T\n", f) // %T输出类型
+	var z = uint(f)       //显性类型转化
 	fmt.Println(x, y, z)
+
+	fmt.Print(Pi)
+
 }
---
+++ src/ch4/for/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	sum2 := 1
+	for sum2 < 100{
+		sum2 += sum2
+	}
+	fmt.Println(sum2)
+}
